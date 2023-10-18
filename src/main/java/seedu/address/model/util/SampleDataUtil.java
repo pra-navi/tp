e5bcce5ac0d40642601356227190bc74682cb135
pra-navi <e0967897@u.nus.edu>
2023-10-18 13:18:28 +0800

    style: Edit indentation

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -5,12 +5,12 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.model.AddressBook;
+import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.task.Note;
 import seedu.address.model.task.Task;
@@ -45,8 +45,8 @@ public class SampleDataUtil {
 
     public static Task[] getSampleTasks() {
         return new Task[]{
-                new Task(new Title("Buy Flowers"), new Note("from Gina")),
-                new Task(new Title("Buy Milk"), new Note("from NTUC"))
+            new Task(new Title("Buy Flowers"), new Note("from Gina")),
+            new Task(new Title("Buy Milk"), new Note("from NTUC"))
         };
     }
 
