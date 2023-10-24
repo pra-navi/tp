78c842ee2dfb3b05d28b3962cfa1136d7c9bc9bc
m1oojv <97022614+m1oojv@users.noreply.github.com>
2023-10-24 17:11:48 +0800

    chore: Add test for findDone

--- src/main/java/seedu/address/logic/commands/FindDoneCommand.java
+++ src/main/java/seedu/address/logic/commands/FindDoneCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.task.Status;
@@ -31,27 +30,5 @@ public class FindDoneCommand extends Command {
         model.updateFilteredTaskList(predicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, model.getFilteredTaskList().size()));
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        // instanceof handles nulls
-        if (!(other instanceof FindDoneCommand)) {
-            return false;
-        }
-
-        FindDoneCommand otherFindDoneCommand = (FindDoneCommand) other;
-        return predicate.equals(otherFindDoneCommand.predicate);
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-                .add("predicate", predicate)
-                .toString();
     }
 }
