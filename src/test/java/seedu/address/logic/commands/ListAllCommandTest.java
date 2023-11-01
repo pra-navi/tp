e7b4abd5215a34824b1e548b70f2d6794ab564cc
jiakai-17 <111171022+jiakai-17@users.noreply.github.com>
2023-11-01 12:31:56 +0800

    docs: Fix wrong comment
    Co-authored-by: Alan Lim <79037826+freshcabbage123@users.noreply.github.com>

--- src/test/java/seedu/address/logic/commands/ListAllCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListAllCommandTest.java
@@ -18,7 +18,7 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
 /**
- * Contains integration tests (interaction with the Model) and unit tests for ListPersonCommand.
+ * Contains integration tests (interaction with the Model) and unit tests for ListAllCommand.
  */
 public class ListAllCommandTest {
 
